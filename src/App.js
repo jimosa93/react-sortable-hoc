141b8636ce5fe304623affece10501277517ef4a
Jorge Morales <jimosa93@gmail.com>
2020-08-13 15:13:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import arrayMove from 'array-move';
+
+const SortableItem = SortableElement(({ value }) => {
+  return (
+    <li className="list-group-item">
+      <h1>{value.title}</h1>
+      <p>{value.description}</p>
+    </li>
+  );
+});
+
+const SortableList = SortableContainer(({ items }) => {
+  return (
+    <ul>
+      {items.map((value, i) => (
+        <SortableItem value={value} key={i} index={i} />
+      ))}
+    </ul>
+  );
+});
+
+function SortableComponent() {
+  const [tasks, setTasks] = useState([
+    { title: 'item 1', description: 'task one' },
+    { title: 'item 2', description: 'task two' },
+    { title: 'item 3', description: 'task three' },
+    { title: 'item 4', description: 'task four' },
+    { title: 'item 5', description: 'task five' }
+  ]);
+
+  const getData = async () => {
+    const res = await fetch('http://localhost:4000/tasks');
+    const tasks = await res.json();
+    tasks.sort((a, b) =>
+      a.sorting > b.sorting ? 1 : b.sorting > a.sorting ? -1 : 0
+    );
+    setTasks(tasks);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const onSortEnd = async ({ oldIndex, newIndex }) => {
+    let tasksCopy = [...tasks];
+    tasksCopy = arrayMove(tasksCopy, oldIndex, newIndex);
+    setTasks(tasksCopy);
+    const tasksIds = tasksCopy.map(t => t._id);
+    const res = await fetch('http://localhost:4000/tasks', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(tasksIds)
+    });
+    const data = await res.json();
+    console.log(data);
+    getData();
+  };
+
+  return <SortableList items={tasks} onSortEnd={onSortEnd} />;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4 offset-md-4">
+          <SortableComponent />
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootswatch/dist/cosmo/bootstrap.min.css';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
